dbb3cc8c1773c530a4d737926ff419a186a10942
rodrigodojo <rodrigodoj@gmail.com>
2021-03-06 12:47:05 -0300

    exemplo de consumer metodo estatico na classe produto

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -26,12 +26,8 @@ public class Product {
 		this.price = price;
 	}
 	
-	public static boolean staticProductPredicate(Product p) {
-		return p.getPrice() >= 100;
-	}
-	
-	public boolean NonStaticProductPredicate() {
-		return price >= 100;
+	public static void staticPriceUpdate(Product p) {
+		p.setPrice(p.getPrice()*1.1);
 	}
 
 	@Override
